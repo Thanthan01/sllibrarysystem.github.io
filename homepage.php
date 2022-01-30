7bd4b5bfeb092c02aaefa2ba8fc269f15c510dc3
Thanthan01 <98628363+Thanthan01@users.noreply.github.com>
2022-01-29 16:07:14 -0800

    New File

---
+++ homepage.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+
+<head>
+  <meta charset="utf-8">
+  <title>IdealLeaf</title>
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+    <a class="navbar-brand" href="#">Ideal leaf</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index2.html">Catalog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index3.html">Contact</a>
+        </li>
+
+
+      </ul>
+
+    </div>
+  </nav>
+
+
+
+ 
+
+</body>
+
+</html>
